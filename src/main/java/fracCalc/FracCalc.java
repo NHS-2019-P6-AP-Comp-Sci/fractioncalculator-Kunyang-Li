374cb84fadc38054ea59e7cb3e7a5ddccfbb62c8
s-liku <s-liku@53S500038274.it.bsd405.org>
2019-11-22 13:15:55 -0800

    Checkpoint 2

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -8,6 +8,13 @@ import java.util.*;
 
 public class FracCalc {
 
+	public static String firstPart;
+	public static String secondPart;
+	public static String thirdPart;
+	public static String first_Part;
+	public static String second_Part;
+	public static String third_Part;
+
 	public static void main(String[] args) {
 		Scanner console = new Scanner(System.in);
 		System.out.println("Please enter here (\"quit\" to quit) : ");
@@ -18,7 +25,6 @@ public class FracCalc {
 			System.out.println("Please enter here (\"quit\" to quit) : ");
 			input = console.nextLine();
 		}
-
 	}
 
 	// TODO: Read the input from the user and call produceAnswer with an equation
@@ -43,41 +49,75 @@ public class FracCalc {
 		String first = input.substring(0, spaceAt);
 		String second = input.substring(spaceAt + 3, length);
 
-		/*int wholeIndex = second.indexOf("_");
+		// second operand
+		int wholeIndex = second.indexOf("_");
 		if (wholeIndex == -1 && second.indexOf("/") == -1) {
-			System.out.print("whole:" + second + " ");
+			firstPart = "whole:" + second + " ";
 		} else if (wholeIndex == -1 && second.indexOf("/") != -1) {
-			System.out.print("whole:0 ");
+			firstPart = "whole:0 ";
 		} else {
-			System.out.print("whole:" + second.charAt(wholeIndex - 1) + " ");
+			firstPart = "whole:" + second.charAt(wholeIndex - 1) + " ";
 		}
 
 		int numerIndex = second.indexOf("/");
 		if (numerIndex == -1 && second.indexOf("_") == -1) {
-			System.out.print("numerator:0 ");
+			secondPart = "numerator:0 ";
 		} else if (numerIndex == -1 && second.indexOf("_") != -1) {
-			System.out.print("numerator:0 ");
+			secondPart = "numerator:0 ";
 		} else if (numerIndex != -1 && second.indexOf("_") == -1) {
-			System.out.print("numerator:" + second.substring(0, numerIndex) + " ");
+			secondPart = "numerator:" + second.substring(0, numerIndex) + " ";
 		} else {
-			System.out.print("numerator:" + second.substring(second.indexOf("_") + 1, second.indexOf("/")) + " ");
+			secondPart = "numerator:" + second.substring(second.indexOf("_") + 1, second.indexOf("/")) + " ";
 		}
 
 		int denomIndex = second.indexOf("/");
 		if (denomIndex == -1 && second.indexOf("_") == -1) {
-			System.out.println("denominator:1");
+			thirdPart = "denominator:1";
 		} else if (denomIndex == -1 && second.indexOf("_") != -1) {
-			System.out.println("denominator:0");
+			thirdPart = "denominator:0";
 		} else if (denomIndex != -1 && second.indexOf("_") == -1) {
-			System.out.println("denominator:" + second.substring(denomIndex + 1, second.length()));
+			thirdPart = "denominator:" + second.substring(denomIndex + 1, second.length());
 		} else {
-			System.out.println("denominator:" + second.substring(denomIndex + 1, second.length()));
-		} */
+			thirdPart = "denominator:" + second.substring(denomIndex + 1, second.length());
+		}
 		
-		return second;
-		
-		
-		
+		// first operand
+		int whole_Index = first.indexOf("_");
+		if (whole_Index == -1 && first.indexOf("/") == -1) {
+			first_Part = "whole:" + first + " ";
+		} else if (whole_Index == -1 && first.indexOf("/") != -1) {
+			first_Part = "whole:0 ";
+		} else {
+			first_Part = "whole:" + first.charAt(whole_Index - 1) + " ";
+		}
+
+		int numer_Index = first.indexOf("/");
+		if (numer_Index == -1 && first.indexOf("_") == -1) {
+			second_Part = "numerator:0 ";
+		} else if (numer_Index == -1 && first.indexOf("_") != -1) {
+			second_Part = "numerator:0 ";
+		} else if (numer_Index != -1 && first.indexOf("_") == -1) {
+			second_Part = "numerator:" + first.substring(0, numer_Index) + " ";
+		} else {
+			second_Part = "numerator:" + first.substring(first.indexOf("_") + 1, first.indexOf("/")) + " ";
+		}
+
+		int denom_Index = first.indexOf("/");
+		if (denom_Index == -1 && first.indexOf("_") == -1) {
+			third_Part = "denominator:1";
+		} else if (denom_Index == -1 && first.indexOf("_") != -1) {
+			third_Part = "denominator:0";
+		} else if (denom_Index != -1 && first.indexOf("_") == -1) {
+			third_Part = "denominator:" + first.substring(denom_Index + 1, first.length());
+		} else {
+			third_Part = "denominator:" + first.substring(denom_Index + 1, first.length());
+		}
+		String ans2 = "";
+		ans2 = firstPart + secondPart + thirdPart;
+
+		String ans1 = "";
+		ans1 = first_Part + second_Part + third_Part;
+		return ans2;
 
 	}
 
